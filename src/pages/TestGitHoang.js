a02620b00587f92ba23378994c49bac576e7fd93
unknown <dnthoang1295@gmail.com>
2024-04-17 20:18:45 +0700

    test Hoang

---
+++ src/pages/TestGitHoang.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TestGitHoang(props) {
+    return (
+        <div>
+            <h1>TestGitHoang</h1>
+        </div>
+    );
+}
+
+export default TestGitHoang;
