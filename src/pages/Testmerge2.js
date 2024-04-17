6ced2cf72818dbf6f6b779dd854c6e3a093d43db
unknown <dnthoang1295@gmail.com>
2024-04-17 20:49:02 +0700

    test merge 2

---
+++ src/pages/Testmerge2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Testmerge2(props) {
+    return (
+        <div>
+            <h1>alo alo merge force</h1>
+        </div>
+    );
+}
+
+export default Testmerge2;
